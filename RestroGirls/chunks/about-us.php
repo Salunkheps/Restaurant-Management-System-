10cc5bf790657ad1dd2a9ffd7450a4a3e15af0b9
PRANAV SURESH SALUNKHE <89740853+Salunkheps@users.noreply.github.com>
2022-06-07 18:03:32 +0530

    Add files via upload

---
+++ RestroGirls/chunks/about-us.php
@@ -0,0 +1,7 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit vero accusantium saepe aperiam beatae enim quidem provident quisquam deserunt, neque autem magni, dolor commodi quasi at atque nam optio quia in. Incidunt ipsum minus saepe placeat consectetur doloribus perferendis illo laborum nam praesentium cupiditate porro tempora corrupti dolores obcaecati quas, deleniti est. </p>
+		      </div>
+	</section>
